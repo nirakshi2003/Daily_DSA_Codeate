2d623abded56ccae3fd3b914b1b11f19fa6ac142
Nirakshi Kundu <96015607+nirakshi2003@users.noreply.github.com>
2024-01-26 17:00:13 +0530

    Update appearing_once.py

--- Day12/appearing_once.py
+++ Day12/appearing_once.py
@@ -1,7 +1,7 @@
 def appearing_once(arr):
     result=0
     for num in arr:
-        result ^= num          #XOR with a number itself results in 0. When we XOR an element that appears twice
+        result ^= num          #XOR with a number itself returns 0. When we XOR an element that appears twice
                                #with the result it cancels out returning 0.
                                #But when we XOR the single number with 0, it remains unchanged.
     return result
