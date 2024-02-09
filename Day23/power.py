68a23b34714b32b231fe1ba5c981502781d13950
Nirakshi Kundu <96015607+nirakshi2003@users.noreply.github.com>
2024-02-09 17:50:40 +0530

    Add files via upload

--- Day23/power.py
+++ Day23/power.py
@@ -1,6 +1,9 @@
 def power(x, n):
-    return pow(x, n)
-
+    return pow(x, n)result = 1
+    for i in range(n):
+        result *= x
+    return result
+    
 x=float(input("Enter the a number : "))
 n=int(input("Enter the power : "))
 print("Output : ", power(x, n))
